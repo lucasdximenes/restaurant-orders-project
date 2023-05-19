a9b87d35581635c9b9a8e7e2101c4db34b6d98d4
Lucas Ximenes <lucasdximenes@gmail.com>
2023-05-19 18:23:18 -0300

    test: :white_check_mark: add `Ingredient` tests

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,22 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, restriction_map  # noqa: F401, E261, E501
+import pytest
 
+
+@pytest.fixture
+def ingredient():
+    return Ingredient("bacon")
+
+
+baconIngredient = Ingredient("bacon")
+cheeseIngredient = Ingredient("cheese")
 
 # Req 1
-def test_ingredient():
-    pass
+
+
+def test_ingredient(ingredient):
+    assert ingredient.name == "bacon"
+    assert ingredient == baconIngredient
+    assert ingredient != cheeseIngredient
+    assert hash(ingredient) == hash(baconIngredient)
+    baconRestrictions = restriction_map().get("bacon", set())
+    assert ingredient.restrictions == baconRestrictions
