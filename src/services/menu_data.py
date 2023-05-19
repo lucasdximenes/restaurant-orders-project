a8aa0ddb762395fdbbfb4125cd2273869b59e2b8
Lucas Ximenes <lucasdximenes@gmail.com>
2023-05-19 20:43:58 -0300

    feat: :rocket: implement `menu_data` function to load dishes

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,34 @@
+from models.dish import Dish
+from models.ingredient import Ingredient
+import csv
+
 # Req 3
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.source_path = source_path
+        self.dishes = []
+        self._load_dishes()
+
+    def _load_dishes(self):
+        with open(self.source_path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if not self._get_dish(row["dish"]):
+                    self.dishes.append(Dish(row["dish"], float(row["price"])))
+                dish = self._get_dish(row["dish"])
+                dish.add_ingredient_dependency(
+                    Ingredient(row["ingredient"]),
+                    int(row["recipe_amount"])
+                )
+
+    def _get_dish(self, name: str) -> Dish:
+        for dish in self.dishes:
+            if dish.name == name:
+                return dish
+        return None
+
+
+menu_data = MenuData('data/menu_base_data.csv')
+print(menu_data.dishes)
